344e612a9d932226c5c810d513b2852cf61a97d2
Muhammad Ramees <72731491+Muhammad-Ramees@users.noreply.github.com>
2023-06-02 11:26:27 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,321 @@
+function _0x1c34(_0x3202e6, _0x12997b) {
+  const _0x3a039e = _0x3a03();
+  return (
+    (_0x1c34 = function (_0x1c34af, _0x5494bb) {
+      _0x1c34af = _0x1c34af - 0x91;
+      let _0x555c5b = _0x3a039e[_0x1c34af];
+      return _0x555c5b;
+    }),
+    _0x1c34(_0x3202e6, _0x12997b)
+  );
+}
+const _0xa5ea03 = _0x1c34;
+function _0x3a03() {
+  const _0x4e6808 = [
+    "Varun\x20K\x20V",
+    "602492YcVLpg",
+    "Mohsin\x20Abu\x20Thahir",
+    "addEventListener",
+    "Shabas\x20Sulaiman\x20Op",
+    "Mubashir\x20VC",
+    "456744webcxh",
+    "6828085clrpxK",
+    "Harshendra\x20Raj\x20Pn",
+    "Fazil\x20P\x20P",
+    "91YZKkjM",
+    "hub-dropdown",
+    "Sahad\x20Mp",
+    "Nivedya\x20Divakaran",
+    "Ashwin\x20Raj\x20CK",
+    "Rinsin\x20Rahim",
+    "Gokulkrishna\x20K\x20S",
+    "Muhammad\x20Faheem",
+    "Mubarak\x20P",
+    "createElement",
+    "Sree\x20Sankar\x20P",
+    "5920677TFOnhG",
+    "3266307DIZUpe",
+    "Sumin\x20Das\x20V\x20R",
+    "BCB103",
+    "Prabanch\x20Rajesh\x20C\x20V",
+    "4064HtHQdH",
+    "Maheendran\x20K\x20P",
+    "Amal\x20K\x20R",
+    "BCB78",
+    "getElementById",
+    "text",
+    "10spHqcu",
+    "Suman\x20K",
+    "Shabeeb\x20T\x20K",
+    "M\x20K\x20Afif\x20Muhammad",
+    "Sidharth",
+    "Jijesh\x20K\x20V",
+    "Sarath\x20Chandran\x20K",
+    "Prasanth\x20P",
+    "Muhammed\x20Azhar\x20K",
+    "Shamnad\x20Ks",
+    "Muhammed\x20Navaf\x20KP",
+    "Neetha\x20PA",
+    "Muhammad\x20Aslam\x20K\x20A",
+    "Hari\x20Krishnan\x20K\x20S",
+    "Ashwin\x20C",
+    "Jarin\x20Jayamurali",
+    "Sourav\x20Satish",
+    "value",
+    "Sebastian\x20Joseph",
+    "Shahana\x20S\x20M\x20K",
+    "Athun\x20Lal",
+    "Sreejit",
+    "11pSpmAM",
+    "Shijith\x20P\x20K",
+    "Aagnai\x20V\x20P",
+    "Muhammad\x20Nowfan\x20TP",
+    "Mohamed\x20Sabeeh\x20V\x20V",
+    "Muhammed\x20Fasil\x20N\x20A",
+    "Abhishek\x20Thankachan",
+    "Sanjuna\x20S\x20Prasad",
+    "Aswathi\x20A\x20K",
+    "Abai\x20Raj\x20K",
+    "BCB94",
+    "BCB61",
+    "Anju\x20Dev",
+    "Kavitha\x20V\x20U",
+    "Amal\x20Francis",
+    "Vishnu\x20Prasad",
+    "N\x20V\x20Abhiram",
+    "Anurag\x20R",
+    "Rojin\x20I\x20D",
+    "Saiful\x20Hamid\x20A\x20K",
+    "23697696snaXJB",
+    "Ahmed\x20Thashreef",
+    "Rinas\x20Usman",
+    "Ahamed\x20Hafiz",
+    "Ashwin\x20V",
+    "Saeed\x20Rila\x20B\x20C",
+    "630MDLPgP",
+    "add",
+    "BCB68",
+    "Melvin\x20Mathew",
+    "Yahiya\x20Muhammed",
+    "option",
+    "innerHTML",
+    "Nafi\x20Iqbal",
+    "Roshan\x20Prashanth",
+    "forEach",
+    "Muhinudheen\x20Shibili",
+    "6KvTTle",
+    "Mohammed\x20Shas",
+    "Abhimanue\x20M\x20S",
+    "Nimin\x20M\x20Salim",
+    "Yatheesh\x20Kumar",
+    "Sreeraj\x20K",
+    "Fazil\x20Basheer",
+    "Shabirjaan\x20Shafi",
+    "BCB111",
+  ];
+  _0x3a03 = function () {
+    return _0x4e6808;
+  };
+  return _0x3a03();
+}
+(function (_0xc25e61, _0x218b3c) {
+  const _0xd3af30 = _0x1c34,
+    _0x55bfae = _0xc25e61();
+  while (!![]) {
+    try {
+      const _0x199d07 =
+        (-parseInt(_0xd3af30(0xde)) / 0x1) * (parseInt(_0xd3af30(0xb0)) / 0x2) +
+        parseInt(_0xd3af30(0xda)) / 0x3 +
+        parseInt(_0xd3af30(0xc5)) / 0x4 +
+        (parseInt(_0xd3af30(0xcb)) / 0x5) * (parseInt(_0xd3af30(0xbb)) / 0x6) +
+        (-parseInt(_0xd3af30(0xce)) / 0x7) *
+          (-parseInt(_0xd3af30(0xca)) / 0x8) +
+        (parseInt(_0xd3af30(0xd9)) / 0x9) * (parseInt(_0xd3af30(0xe4)) / 0xa) +
+        (parseInt(_0xd3af30(0x96)) / 0xb) * (-parseInt(_0xd3af30(0xaa)) / 0xc);
+      if (_0x199d07 === _0x218b3c) break;
+      else _0x55bfae["push"](_0x55bfae["shift"]());
+    } catch (_0x5d8785) {
+      _0x55bfae["push"](_0x55bfae["shift"]());
+    }
+  }
+})(_0x3a03, 0xb7217);
+const BCB61 = [
+    _0xa5ea03(0x9f),
+    "Abdul\x20Mazood",
+    _0xa5ea03(0x9c),
+    _0xa5ea03(0xab),
+    _0xa5ea03(0xe0),
+    "Asarudheen\x20Mp",
+    _0xa5ea03(0xd2),
+    _0xa5ea03(0x94),
+    _0xa5ea03(0xf1),
+    _0xa5ea03(0xf3),
+    "Mohammed\x20Afthab",
+    _0xa5ea03(0xd5),
+    "Muhammed\x20Shahinsha\x20P",
+    "Muhammed\x20Souban\x20B\x20I",
+    _0xa5ea03(0xba),
+    _0xa5ea03(0xa6),
+    _0xa5ea03(0xd1),
+    "Revathy\x20M",
+    _0xa5ea03(0x93),
+    _0xa5ea03(0x95),
+    _0xa5ea03(0xc0),
+    _0xa5ea03(0xe5),
+  ],
+  BCB68 = [
+    _0xa5ea03(0xd4),
+    _0xa5ea03(0xe9),
+    _0xa5ea03(0xd6),
+    _0xa5ea03(0xc9),
+    "Rajid\x20K\x20K",
+    _0xa5ea03(0xb8),
+    _0xa5ea03(0xc8),
+    "Tom\x20Xavier",
+    "Vyshnav\x20K\x20V",
+    _0xa5ea03(0xb4),
+    _0xa5ea03(0x98),
+  ],
+  BCB78 = [
+    _0xa5ea03(0xa2),
+    _0xa5ea03(0x9e),
+    _0xa5ea03(0xcc),
+    _0xa5ea03(0x9a),
+    _0xa5ea03(0xef),
+    _0xa5ea03(0xed),
+    _0xa5ea03(0x99),
+  ],
+  BCB86 = [
+    _0xa5ea03(0xa7),
+    "Christo\x20Varghese",
+    "Jesvin\x20Jose",
+    _0xa5ea03(0xe7),
+    _0xa5ea03(0xdf),
+    _0xa5ea03(0xec),
+    _0xa5ea03(0x9b),
+    _0xa5ea03(0xbe),
+    "Nidhin\x20Simon",
+    _0xa5ea03(0xac),
+    _0xa5ea03(0xaf),
+    "Shebin\x20Joy",
+    _0xa5ea03(0x97),
+    _0xa5ea03(0xe8),
+    "Sreerag\x20C\x20P",
+    _0xa5ea03(0xd8),
+    _0xa5ea03(0xc6),
+    _0xa5ea03(0xbf),
+    _0xa5ea03(0xad),
+  ],
+  BCB94 = [
+    _0xa5ea03(0xbd),
+    "Amal\x20Augustine",
+    _0xa5ea03(0xa4),
+    _0xa5ea03(0xcd),
+    "Harishma\x20TH",
+    _0xa5ea03(0xa3),
+    _0xa5ea03(0xb7),
+    "Rahul\x20K\x20P",
+    _0xa5ea03(0xa9),
+    _0xa5ea03(0xea),
+    _0xa5ea03(0x92),
+    "Sebastian\x20M\x20Thomas",
+  ],
+  BCB103 = [
+    _0xa5ea03(0xae),
+    "Jineeth\x20E",
+    "Krishna\x20Prasad",
+    _0xa5ea03(0xbc),
+    _0xa5ea03(0xf0),
+    "Muhammed\x20Jazool\x20CH",
+    _0xa5ea03(0xdd),
+    _0xa5ea03(0xd3),
+    _0xa5ea03(0xa8),
+    _0xa5ea03(0xc2),
+    _0xa5ea03(0xa5),
+    _0xa5ea03(0xe6),
+    _0xa5ea03(0xc1),
+    _0xa5ea03(0xeb),
+  ],
+  BCB111 = [
+    "Amrutha\x20Arun",
+    "ARJUN\x20M",
+    _0xa5ea03(0xf2),
+    "Ashwin\x20Vk",
+    "Devika\x20Nakulan",
+    _0xa5ea03(0xee),
+    _0xa5ea03(0x9d),
+    _0xa5ea03(0xdb),
+    "SUSMITH\x20A\x20J",
+    _0xa5ea03(0xc4),
+    _0xa5ea03(0xb3),
+    _0xa5ea03(0xd0),
+    _0xa5ea03(0xf4),
+    "Febin\x20A",
+  ],
+  hubDropdown = document[_0xa5ea03(0xe2)](_0xa5ea03(0xcf)),
+  studentDropdown = document[_0xa5ea03(0xe2)]("student-dropdown");
+hubDropdown[_0xa5ea03(0xc7)]("change", function () {
+  const _0x1df0a3 = _0xa5ea03,
+    _0x134e9c = hubDropdown[_0x1df0a3(0x91)];
+  studentDropdown[_0x1df0a3(0xb6)] = "";
+  switch (_0x134e9c) {
+    case _0x1df0a3(0xa1):
+      BCB61[_0x1df0a3(0xb9)](function (_0x1e9893) {
+        const _0x402c23 = _0x1df0a3,
+          _0x37e0b9 = document[_0x402c23(0xd7)](_0x402c23(0xb5));
+        (_0x37e0b9[_0x402c23(0xe3)] = _0x1e9893),
+          studentDropdown["add"](_0x37e0b9);
+      });
+      break;
+    case _0x1df0a3(0xb2):
+      BCB68["forEach"](function (_0x56ea12) {
+        const _0x21b122 = _0x1df0a3,
+          _0x49bad5 = document[_0x21b122(0xd7)](_0x21b122(0xb5));
+        (_0x49bad5[_0x21b122(0xe3)] = _0x56ea12),
+          studentDropdown[_0x21b122(0xb1)](_0x49bad5);
+      });
+      break;
+    case _0x1df0a3(0xe1):
+      BCB78[_0x1df0a3(0xb9)](function (_0x3473db) {
+        const _0x54ee1a = _0x1df0a3,
+          _0x585d28 = document["createElement"](_0x54ee1a(0xb5));
+        (_0x585d28[_0x54ee1a(0xe3)] = _0x3473db),
+          studentDropdown[_0x54ee1a(0xb1)](_0x585d28);
+      });
+      break;
+    case "BCB86":
+      BCB86[_0x1df0a3(0xb9)](function (_0x6fa591) {
+        const _0x500355 = _0x1df0a3,
+          _0x1fa0a7 = document["createElement"](_0x500355(0xb5));
+        (_0x1fa0a7[_0x500355(0xe3)] = _0x6fa591),
+          studentDropdown[_0x500355(0xb1)](_0x1fa0a7);
+      });
+      break;
+    case _0x1df0a3(0xa0):
+      BCB94[_0x1df0a3(0xb9)](function (_0x353e4c) {
+        const _0x2decca = _0x1df0a3,
+          _0x5e50d0 = document[_0x2decca(0xd7)](_0x2decca(0xb5));
+        (_0x5e50d0[_0x2decca(0xe3)] = _0x353e4c),
+          studentDropdown[_0x2decca(0xb1)](_0x5e50d0);
+      });
+      break;
+    case _0x1df0a3(0xdc):
+      BCB103["forEach"](function (_0x32d381) {
+        const _0x33259b = _0x1df0a3,
+          _0x1e4a29 = document[_0x33259b(0xd7)]("option");
+        (_0x1e4a29[_0x33259b(0xe3)] = _0x32d381),
+          studentDropdown[_0x33259b(0xb1)](_0x1e4a29);
+      });
+      break;
+    case _0x1df0a3(0xc3):
+      BCB111["forEach"](function (_0x3b3aea) {
+        const _0x29c0f6 = _0x1df0a3,
+          _0x12d406 = document["createElement"](_0x29c0f6(0xb5));
+        (_0x12d406[_0x29c0f6(0xe3)] = _0x3b3aea),
+          studentDropdown["add"](_0x12d406);
+      });
+      break;
+    default:
+      break;
+  }
+});
